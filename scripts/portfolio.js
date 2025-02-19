4ff1babacd35569fa90ffe039a47b0736474a87a
Jason111nn <126970887+jason111nn@users.noreply.github.com>
2025-02-19 16:03:15 +0800

    Add files via upload

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -12,15 +12,38 @@ $(document).ready(function () {
                 let item = $("<div>").addClass("portfolio-item");
                 item.append(`<h2>${repo.name}</h2>`);
                 item.append(`<span>最後更新: ${new Date(repo.updated_at).toLocaleDateString()}</span>`);
+                item.append(`<span>星標數: ${repo.stargazers_count}</span>`);
+                item.append(`<span>Fork 數: ${repo.forks_count}</span>`);
+                item.append(`<span>開放議題數: ${repo.open_issues_count}</span>`);
+                if (repo.description) {
+                    item.append(`<p>${repo.description}</p>`);
+                }
+                item.append(`<span>創建時間: ${new Date(repo.created_at).toLocaleDateString()}</span>`);
+
+                // 顯示版本
+                $.get(`https://api.github.com/repos/jason111nn/${repo.name}/releases`, function (releases) {
+                    if (releases.length > 0) {
+                        item.append(`<span>最新版本: ${releases[0].tag_name}</span>`);
+                    }
+                });
+
+                // 顯示貢獻者
+                $.get(`https://api.github.com/repos/jason111nn/${repo.name}/contributors`, function (contributors) {
+                    if (contributors.length > 0) {
+                        let contributorsList = contributors.map(function (contributor) {
+                            return contributor.login;
+                        }).join(", ");
+                        item.append(`<span>貢獻者: ${contributorsList}</span>`);
+                    }
+                });
 
                 let img = $("<img>").attr({ src: imageUrl, alt: imageUrl });
                 img.on("error", function () {
-                    $(this).attr("src", `./portfolio.jpg`); // 預設圖片
+                    $(this).attr("src", `./img/error-portfolio.jpg`); // 預設圖片
                 });
                 item.prepend(img);
 
                 let buttonContainer = $("<div>");
-
                 let repoBtn = $("<a>")
                     .attr("href", repo.html_url)
                     .attr("target", "_blank")
@@ -30,7 +53,8 @@ $(document).ready(function () {
                     .text("線上 Demo")
                     .click(function () {
                         $("#iframe-viewer").attr("src", rawHtmlUrl);
-                        $("#iframe-container").fadeIn();
+                        $("body").css("overflow","none");
+                        $("#iframe-container").fadeIn().css('display', 'flex');
                     });
 
                 buttonContainer.append(repoBtn, demoBtn);
@@ -42,6 +66,6 @@ $(document).ready(function () {
             }
         });
     }).fail(function () {
-        container.html("<p>無法載入作品集，請稍後再試。</p>");
+        container.html("<div></div><p>無法載入作品集，請稍後再試。</p>");
     });
 });
